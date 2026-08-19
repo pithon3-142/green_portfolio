768d42deca5a83e298d83bb7e41242d9efa00a8a
pithon3-142 <pithon3.142@gmail.com>
2026-08-19 16:47:02 +0200

    eperimenting on jQuery

--- js/animations.js
+++ js/animations.js
@@ -1,14 +1,19 @@
 // random orb movement
 const orbs = document.querySelectorAll('.orb');
 
-function orbMovement() {
+    function orbMovement() {
     orbs.forEach(orb => {
         const x = Math.random() * window.innerWidth;
         const y = Math.random() * window.innerHeight;
         orb.style.transform = `translate(${x}px, ${y}px)`;
-    });
-}
+        });
+    }
 
-orbMovement();
+    orbMovement();
 
 setInterval(orbMovement, 5000);
+
+//some simple jQuery imlementation
+$(document).ready(function () {
+    $("#home").hide().fadeIn(3000);
+});
